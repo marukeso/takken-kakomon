f54570cee422e1f41c417d876d6a9391660b6320
Kentaro Maruyama <marukeso@gmail.com>
2022-04-11 20:09:13 +0900

    style: add exam icon

--- components/YearList.tsx
+++ components/YearList.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { createHasuraClient } from '../utils/hasuraClient'
 import { useQuery } from 'react-query'
 import { Loading } from './Loading'
+import Image from 'next/image'
 
 const YearList: VFC = () => {
   const hasuraClient = createHasuraClient(null)
@@ -15,6 +16,7 @@ const YearList: VFC = () => {
     <div className="grid grid-cols-1 gap-6 px-4 md:grid-cols-2 lg:grid-cols-4 lg:px-0">
       {data?.years.map((year) => (
         <div className="card space-y-4 pt-5 pb-7 font-medium">
+          <Image src="/exam.svg" width={40} height={40} alt="PC画面" />
           <p className="text-center text-lg">{year.content}</p>
           <Link href={`/year/${year.id}`}>
             <a className="btn btn-accent w-full border-none bg-opacity-60">
