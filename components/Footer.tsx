4fb0ffbac609e53911d486434f0a656e74532b68
Kentaro Maruyama <marukeso@gmail.com>
2022-04-11 15:07:13 +0900

    style: footer responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,9 @@ import { VFC } from 'react'
 
 export const Footer: VFC = () => {
   return (
-    <footer className="bg-base-100 py-6">
-      <div className="mx-auto flex max-w-[1000px] items-center text-sm">
-        <div className="flex space-x-6">
+    <footer className="bg-base-100 py-6 px-4 lg:px-0">
+      <div className=" mx-auto flex max-w-[1000px] flex-col items-start text-sm lg:flex-row lg:items-center">
+        <div className="flex flex-col lg:flex-row lg:space-x-6">
           <Link href="/terms">
             <a className="hover:opacity-60">利用規約</a>
           </Link>
@@ -18,7 +18,9 @@ export const Footer: VFC = () => {
           </Link>
         </div>
 
-        <p className="ml-auto text-xs">©2022 宅建過去問</p>
+        <p className="mt-10 w-full text-center text-xs lg:mt-0 lg:ml-auto lg:w-max">
+          ©2022 宅建過去問
+        </p>
       </div>
     </footer>
   )
