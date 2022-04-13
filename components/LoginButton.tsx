0f7535deb8ca2f8939efed7e9f5a06ba6fe6dd1e
Kentaro Maruyama <marukeso@gmail.com>
2022-04-13 15:34:54 +0900

    fix: localstorage answer list to be empty when logout
    Closes: #60

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,9 +1,19 @@
 import Link from 'next/link'
 import { VFC } from 'react'
 import { useUser } from '@auth0/nextjs-auth0'
+import Router from 'next/router'
+import { useRecoilState } from 'recoil'
+import { answerListState } from 'atoms/answerLIstAtom'
 
 export const LoginButton: VFC = () => {
   const { user } = useUser()
+  const [answerList, setAnswerList] = useRecoilState(answerListState)
+
+  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    setAnswerList([])
+    Router.push('/api/auth/logout')
+  }
 
   return user ? (
     <div className="dropdown-end dropdown">
@@ -22,9 +32,7 @@ export const LoginButton: VFC = () => {
         className="dropdown-content menu rounded-box mt-2 w-52 bg-base-100 p-2 shadow-lg"
       >
         <li>
-          <Link href="/api/auth/logout">
-            <a>ログアウト</a>
-          </Link>
+          <button onClick={handleOnClick}>ログアウト</button>
         </li>
       </ul>
     </div>
