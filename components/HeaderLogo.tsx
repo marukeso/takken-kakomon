3eb48419c5ad150da7bd29e5ca534f70f9934b51
Kentaro Maruyama <marukeso@gmail.com>
2022-04-14 10:16:46 +0900

    style: fix logo size
    Closes: #70

--- components/HeaderLogo.tsx
+++ components/HeaderLogo.tsx
@@ -6,9 +6,9 @@ export const HeaderLogo: VFC = () => {
   return (
     <h1>
       <Link href="/">
-        <a className="text-noto flex items-center text-2xl font-light">
+        <a className="text-noto flex items-center text-[20px] font-light">
           <Image src="/logo.svg" width={25} height={26} alt="宅建過去問" />
-          <span className="ml-2">宅建過去問</span>
+          <span className="relative top-[1px] ml-2">宅建過去問</span>
         </a>
       </Link>
     </h1>
